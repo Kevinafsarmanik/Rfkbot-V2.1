c43559320e58b6fb01fd759bb907da15c4a40939
Oғғɪᴄɪᴀʟ Bᴏᴛ Lʏɴ乂ᴢʏ   ? <98347766+NoDigi@users.noreply.github.com>
2022-03-08 00:41:54 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,16 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Pin 〕
-├ Digi🥀 : 60149431385
-├ Umobile🥀 : 60189830350
-├ 𝐆𝐑𝐎𝐔𝐏 𝐁𝐎𝐓 𝐋𝐘𝐍¹🥀
-├ https://chat.whatsapp.com/EheAWPrQMhV25xW0N7l7WD
-├ 𝐆𝐑𝐎𝐔𝐏 𝐁𝐎𝐓 𝐋𝐘𝐍²🥀
-├ https://chat.whatsapp.com/GzDQlgoUamS5Ok4EOcYRRB
+├ Tng🥀 : 0139407383
+├ Boost🥀 : 0139407383
 └────
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
-
 module.exports = handler
